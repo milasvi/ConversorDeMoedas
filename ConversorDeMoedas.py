b428283fe99b8249b3752c6943ce1332838f35ef
Milena Sviatek <81256013+milasvi@users.noreply.github.com>
2022-05-13 09:22:25 -0300

    Update ConversorDeMoedas.py

--- ConversorDeMoedas.py
+++ ConversorDeMoedas.py
@@ -4,10 +4,10 @@ vezes = int(input('Bem-vinde ao conversor de moedas, você quer converter quanto
 moeda = input('Por favor, escolha qual moeda você quer converter: \nDólar[1];\nYenes[2];\nLibra esterlina[3].')
 
 if moeda == '1':
-    print('Bem vinde ao conversor de Real para Dólar!')
+    print('Bem-vinde ao conversor de Real para Dólar!')
     while contador < vezes:
         contador = contador + 1
-        Real = int(input(f'Qual o {contador}º valor em R$?'))
+        Real = float(input(f'Qual o {contador}º valor em R$?'))
         Conversao = 5 * Real
         print(f'Esse valor fica {Conversao} dólares.')
     print('Obrigade por utilizar o Conversor de Moedas! \nAté mais!')
@@ -16,7 +16,7 @@ if moeda == '2':
     print('Bem vinde ao conversor de Real para Yene!')
     while contador < vezes:
         contador = contador + 1
-        Real = int(input(f'Qual o {contador}º valor em R$?'))
+        Real = float(input(f'Qual o {contador}º valor em R$?'))
         Conversao = 25.62 * Real
         print(f'Esse valor fica {Conversao} yenes.')
     print('Obrigade por utilizar o Conversor de Moedas! \nAté mais!')
@@ -25,7 +25,7 @@ if moeda == '3':
     print('Bem vinde ao conversor de Real para Libras Esterlinas!')
     while contador < vezes:
         contador = contador + 1
-        Real = int(input(f'Qual o {contador}º valor em R$?'))
+        Real = float(input(f'Qual o {contador}º valor em R$?'))
         Conversao = 6 * Real
         print(f'Esse valor fica {Conversao} libras esterlinas.')
     print('Obrigade por utilizar o Conversor de Moedas! \nAté mais!')
